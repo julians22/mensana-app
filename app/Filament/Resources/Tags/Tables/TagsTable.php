80aa5bc3eb914df6ef254c08075619da855f67d9
julians22 <dabnerjulian@yahoo.com>
2025-12-10 17:54:09 +0700

    fix tag products

--- app/Filament/Resources/Tags/Tables/TagsTable.php
+++ app/Filament/Resources/Tags/Tables/TagsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Tags\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -14,8 +15,11 @@ class TagsTable
     {
         return $table
             ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
                 TextColumn::make('slug')
                     ->searchable(),
+                ColorColumn::make('color'),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
